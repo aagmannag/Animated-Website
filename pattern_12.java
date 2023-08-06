d7f17afa95c6fc2a8fa8a3fd33ab30b2e087e5e9
Aagman nag <118126919+aagmannag@users.noreply.github.com>
2023-08-06 14:16:45 +0530

    Add files via upload

---
+++ pattern_12.java
@@ -0,0 +1,16 @@
+// import java.util.*;
+public class pattern_12{
+    public static void main(String[] args) {
+        int n=5;
+        for (int i=1;i<=5;i++){
+            for (int j=5;j>=i;j--){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println(" ");
+        }
+        System.out.println(n);
+    }
+}
---
+++ pattern_13.java
@@ -0,0 +1,18 @@
+// import java.util.*;
+public class pattern_13{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for (int j=(n-1);j>=i;j--){
+                System.out.print(" ");
+            }
+            for (int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            for (int j=2;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println(" ");
+        }
+    }
+}
